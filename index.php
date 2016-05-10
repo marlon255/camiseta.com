fd5426ebc81ff89b5db03c1263981343f2466e7a
marlon255 <marlon.0255@gmail.com>
2016-05-09 22:34:13 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Sistema para controle de estoque</title>
+<link rel="stylesheet" type="text/css" href="css/estilo.css">
+<script type="text/javascript"></script>
+</head>
+<body>
+	<div class="topo">
+		<div class="logo">
+		</div>
+		<div class="name">
+			<h2>Camiseta.com</h2>
+			<h3>Feita para você!</h3>
+		</div>
+		<div class="usuario">
+			<h2>Usuario online.</h2>
+		</div>
+		<div class="img_user">
+		</div>
+	</div>
+	<div class="menu">
+		<div>
+			<span>Inicio</span>
+		</div>
+		<div class="drop">
+			<span>Produto</span>
+			<ul class="dropdown">
+				<li>Material</li>
+				<li>Funcionario</li>
+				<li>Despesas</li>
+			</ul>
+		</div>
+		<div class="drop">
+			<span>Caixa</span>
+			<ul class="dropdown">
+				<li>Entrada</li>
+				<li>Saída</li>
+			</ul>
+		</div>
+		<div>
+			<span>Estoque</span>
+		</div>
+	</div>
+	<div class="content">
+		<div class="conteudo">
+			Bem vindo ao Sistema de controle de estoque da empresa Camiseta.com!<br>
+			Aqui você poderá realizar todo o controle de estoque e caixa.<br>
+			Bom uso!
+		</div>
+	</div>
+</body>
+</html>
