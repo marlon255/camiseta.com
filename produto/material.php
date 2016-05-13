cf6e0120f7a20aee54eb43937e95249ecc23551a
marlon255 <marlon.0255@gmail.com>
2016-05-13 15:35:44 -0300

    update

--- produto/material.php
+++ produto/material.php
@@ -7,11 +7,11 @@
 <form class="cadastro">
 	<div class="lcadastro">
 		<label>Material</label>
-		<input type="text" required>
+		<input type="text" class="text" required>
 	</div>
 	<div class="lcadastro">
 		<label>Tipo</label>
-		<input type="text" required>
+		<input type="text" class="text" required>
 	</div>
 	<div class="lcadastro">
 		<label>Grupo</label>
@@ -22,7 +22,7 @@
 		</select>
 	</div>
 	<div>
-		<input type="submit">
+		<input type="submit" class="button" value="Cadastrar">
 	</div>
 </form>
 <div class="exibir">
@@ -33,9 +33,15 @@
 		<div>Ações</div>
 	</div>
 	<div class="material">
-		<input type="text" required>
-		<input type="text" required>
-		<input type="text" required>
+		<input type="text" class="edit" disabled>
+		<input type="text" class="edit" disabled>
+		<select disabled>
+			<option>Selecione-->></option>
+			<option>Selecione-->>></option>
+		</select>
+		<input type="submit" id="editar" class="bt_edit" value="" title="Editar" required>
+		<input type="submit" id="salvar" class="bt_edit" value="" title="Salvar" required>
+		<input type="submit" id="excluir" class="bt_edit" value="" title="Deletar" required>
 	</div>
 </div>
 <?php
